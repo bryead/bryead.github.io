45b64eff9c92d7d612978a4daf06734b542780d3
Bryead <bryantan3011@live.com>
2025-05-05 14:21:53 +0800

    reset

---
+++ public/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.getElementById("modeToggle");
+    toggle.addEventListener("click", () => {
+      document.body.classList.toggle("dark-mode");
+    });
+  });
+  
